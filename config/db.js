99ed50d49584260b4a8245d2e156023d978f9a05
Evans Obeng <iamevansobeng@outlook.com>
2020-10-07 19:25:49 +0000

    Refactored dbjs of mongo connection to use async

--- config/db.js
+++ config/db.js
@@ -4,17 +4,18 @@ const config = require('config')
 const db = config.get('mongoURI')
 
 
-const connectDB = () => {
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    }).then(() => console.log('MongoDB CONNECTED'))
-        .catch(err => {
-            console.error(err.message)
-            process.exit(1)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
         })
+        console.log("Mongo DB Connected");
+    } catch (error) {
+        console.error(error.message);
+    }
 
 }
 module.exports = connectDB
