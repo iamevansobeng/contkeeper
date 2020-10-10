3ffae31c9f9a739cb28cf9619595b6a6b1ba425a
Evans Obeng <iamevansobeng@outlook.com>
2020-10-10 14:45:28 +0000

    User created , password hashes, validation done and user saved to db

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 const router = express.Router()
@@ -13,8 +14,43 @@ router.post('/', [
     check('name', 'Please your name is required').not().isEmpty(),
     check('email', 'Please include a valid Email').isEmail(),
     check('password', "Please enter a password with 6 or more characters").isLength({ min: 6 })
-], (req, res) => {
-    res.send(req.body)
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, password } = req.body
+
+    try {
+        // CHECH WHETER THER EMAIL IS ONLY ONE IN THE DATABSE (is unique)
+        let user = await User.findOne({ email })
+
+        // send error message if user exists 
+        if (user) {
+            res.status(400).json({ msg: "User already exists" })
+        }
+
+        // Pass info to a new user ( create a news user )
+        user = new User({
+            name,
+            email,
+            password
+        })
+
+        // Hashing the password 
+        // generation a salt and using the salt
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+
+        // saving the user
+        await user.save()
+        res.send('User saved')
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
