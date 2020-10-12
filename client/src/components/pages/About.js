936db15fb2cc6e652af49075e9c8642fedd41f43
Evans Obeng <iamevansobeng@outlook.com>
2020-10-12 13:41:56 +0000

    Added routes links to navbar

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <h1>About This App</h1>
             <p className="my-1">
-                This is a fullstact react app to save contacts
+                This is a fullstack react app to save contacts
             </p>
             <p className="bg-dark p">
                 <strong>Version : 0.1.0</strong>
