d4e98498614fe731c3de8622b8c5633db71db1f5
Evans Obeng <iamevansobeng@outlook.com>
2020-10-14 15:23:23 +0000

    Css Group Transitions Implimented

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import ContactContext from '../../context/contact/contactContext'
 import ContactItem from './ContactItem'
 
@@ -11,10 +12,18 @@ const Contacts = () => {
     }
 
     return (
-        <>  {filtered !== null
-            ? filtered.map(contact => <ContactItem key={contact.id} contact={contact} />)
-            : contacts.map(contact => <ContactItem key={contact.id} contact={contact} />
-            )}
+        <>
+            <TransitionGroup>
+                {filtered !== null
+                    ? filtered.map(contact =>
+                        <CSSTransition key={contact.id} timeout={350} classNames='item'>
+                            <ContactItem contact={contact} />
+                        </CSSTransition>)
+                    : contacts.map(contact =>
+                        <CSSTransition key={contact.id} timeout={350} classNames='item'>
+                            <ContactItem contact={contact} />
+                        </CSSTransition>)}
+            </TransitionGroup>
         </>
     )
 }
